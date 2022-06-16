5422f465dc5c55a7922f11955aad1a23bb1d13c4
ianflynnwork <ianflynnwork@gmail.com>
2022-06-16 16:46:14 -0400

    js changes

--- index.js
+++ index.js
@@ -1,15 +1,17 @@
-let navToggle = document.querySelector('.nav-toggle');
-let navLinks = document.querySelectorAll('.nav__link')
-navToggle.addEventListener('click', () => {
-  document.body.classList.toggle('.nav-open');
+window.onload=function(){
+  let navToggle = document.querySelector('.nav-toggle');
+  let navLinks = document.querySelectorAll('.nav__link')
+  navToggle.addEventListener('click', () => {
+    document.body.classList.toggle('nav-open');
+  })
+  navLinks.forEach(link => {
+    link.addEventListener('click', () => {
+        document.body.classList.remove('nav-open');
+    })
 })
+}
 /*navLinks.forEach(link => {
   link.addEventListener('click', () => {
     document.body.classList.remove('nav-open');
   });
 };*/
-navLinks.forEach(link => {
-    link.addEventListener('click', () => {
-        document.body.classList.remove('.nav-open');
-    })
-})
